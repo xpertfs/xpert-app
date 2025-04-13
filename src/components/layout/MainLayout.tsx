d16cd64c118b508d2aecb851dcdbc94b251e5d33
xpertfs <irayhan@xpertfieldservices.com>
2025-04-12 22:52:03 -0400

    Add logout option

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,9 +1,12 @@
 // src/components/layout/MainLayout.tsx
 import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Box, CssBaseline, Drawer, AppBar, Toolbar, Typography, IconButton, Avatar, useTheme } from '@mui/material';
-import { Menu as MenuIcon, ChevronLeft, Notifications, DarkMode, LightMode } from '@mui/icons-material';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Box, CssBaseline, Drawer, AppBar, Toolbar, Typography, IconButton, Avatar, Menu, MenuItem, useTheme } from '@mui/material';
+import { Menu as MenuIcon, ChevronLeft, Notifications, DarkMode, LightMode, ExitToApp } from '@mui/icons-material';
 import { styled } from '@mui/material/styles';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../../features/store';
+import { logout } from '../../features/auth/authSlice';
 import Sidebar from './Sidebar';
 
 const drawerWidth = 260;
@@ -18,7 +21,24 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 
 const MainLayout = () => {
   const [open, setOpen] = useState(true);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
   const theme = useTheme();
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/login');
+    handleMenuClose();
+  };
 
   return (
     <Box sx={{ display: 'flex', backgroundColor: theme.palette.background.default, minHeight: '100vh' }}>
@@ -47,9 +67,32 @@ const MainLayout = () => {
           <IconButton sx={{ ml: 1 }}>
             <DarkMode />
           </IconButton>
-          <IconButton sx={{ ml: 1 }}>
+          <IconButton 
+            sx={{ ml: 1 }}
+            onClick={handleMenuOpen}
+          >
             <Avatar sx={{ width: 32, height: 32, bgcolor: 'primary.main' }}>JD</Avatar>
           </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleMenuClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Account Settings</MenuItem>
+            <MenuItem onClick={handleLogout}>
+              <ExitToApp sx={{ mr: 1 }} fontSize="small" />
+              Logout
+            </MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
       <Drawer
